d881ed210143af0f6dab2fac4c7718de22d6f8e9
sabkazz <90966310+KazikS@users.noreply.github.com>
2024-10-28 16:17:24 +0300

    add game page controller

---
+++ Indie Game Space/Project/IGS/Controllers/GamePageController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace IGS.Controllers
+{
+    public class GamePageController : Controller
+    {
+        public IActionResult GamePage()
+        {
+            return View();
+        }
+    }
+}
--- Indie Game Space/Project/IGS/obj/Debug/net6.0/IGS.AssemblyInfo.cs
+++ Indie Game Space/Project/IGS/obj/Debug/net6.0/IGS.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("IGS")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fe953d52e08d9de7d1b8d6df12f41327a01a9659")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+defe3e024b578ea980bad84c6d8a5b1e28d7e60f")]
 [assembly: System.Reflection.AssemblyProductAttribute("IGS")]
 [assembly: System.Reflection.AssemblyTitleAttribute("IGS")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
