dc417de70ee0967a20908b4e3ddba35c962a999c
sabkazz <90966310+KazikS@users.noreply.github.com>
2025-03-11 16:09:21 +0300

    update jam's pages

--- Indie Game Space/Project/IGS.Service/obj/Debug/net6.0/IGS.Service.AssemblyInfo.cs
+++ Indie Game Space/Project/IGS.Service/obj/Debug/net6.0/IGS.Service.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("IGS.Service")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4b48270eca6ac5540fccdb4b5f8a37a83ff615a2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+219ff935e5b4bf938af00369fd01bed0c1afa492")]
 [assembly: System.Reflection.AssemblyProductAttribute("IGS.Service")]
 [assembly: System.Reflection.AssemblyTitleAttribute("IGS.Service")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
