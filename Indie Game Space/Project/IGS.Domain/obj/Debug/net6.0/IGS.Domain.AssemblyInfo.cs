205cc167330d81cf54c65fea06b12d3d259ffdc1
sabkazz <90966310+KazikS@users.noreply.github.com>
2025-03-22 02:04:10 +0300

    fix screenshots layout on addGamePage

--- Indie Game Space/Project/IGS.Domain/obj/Debug/net6.0/IGS.Domain.AssemblyInfo.cs
+++ Indie Game Space/Project/IGS.Domain/obj/Debug/net6.0/IGS.Domain.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("IGS.Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0d8d6228a716bc3636af6a66b9acb0792b4b1254")]
 [assembly: System.Reflection.AssemblyProductAttribute("IGS.Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("IGS.Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
